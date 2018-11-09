1c07ace8a4089405fc9b7076293853dadebf013d
Natalia Garcia Martin <nmartin@tragopan.stats.ox.ac.uk>
2018-11-09 17:05:10 +0000

    146 genes dataset obtained from main dataset

--- Code/loaddata.R
+++ Code/loaddata.R
@@ -7,3 +7,7 @@ load("Data/gset.RData")
 # Read in data using only 146 significant genes
 
 pamoutput <- read.csv("Data/desousaemelo_146genes_pam.csv", header = TRUE)
+
+# Subset with 146 genes
+
+x146<- gset[as.character(pamoutput$identifier),]
