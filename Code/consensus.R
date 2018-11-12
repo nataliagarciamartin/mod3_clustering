b75cab31a2fc7954337c934e255eaf5e1c0b5c5a
Natalia Garcia Martin <nmartin@tragopan.stats.ox.ac.uk>
2018-11-12 18:17:57 +0000

    proportion of data for bootstrapping 0.98

--- Code/consensus.R
+++ Code/consensus.R
@@ -1,12 +1,6 @@
 library(ConsensusClusterPlus)
-title=tempdir()
-results = ConsensusClusterPlus(mad_gset,maxK=3,reps=50,pItem=0.8,pFeature=1,distance="pearson",seed=5)
-                           #   + title=title,clusterAlg="hc",distance="pearson",seed=1262118388.71279,plot="png")
-#consensusTree - hclust object
-results[[2]][["consensusTree"]]
-#ml - consensus matrix result
-#clrs - colors for cluster
-icl = calcICL(results,title=title,plot="png")
+results = ConsensusClusterPlus(mad_gset, maxK=3, reps=1000, pItem=0.98, pFeature=1, distance="pearson", seed=5, clusterAlg="hc")
+icl = calcICL(results)
 icl[["clusterConsensus"]]
 icl[["itemConsensus"]][1:5,]
 
@@ -14,22 +8,6 @@ sum(results[[3]]$consensusClass==1)/90
 sum(results[[3]]$consensusClass==2)/90
 sum(results[[3]]$consensusClass==3)/90
 
-results[[3]]$consensusClass
+results[[3]]$consensusClass  # class results for 3 clusters
 
 
-
-# check if it matches the plot 
-
-results = ConsensusClusterPlus(x146, ,maxK=3,reps=50,pItem=0.8,pFeature=1)
-#    + title=title,clusterAlg="hc",distance="pearson",seed=1262118388.71279,plot="png")
-#consensusTree - hclust object
-results[[2]][["consensusTree"]]
-#ml - consensus matrix result
-#clrs - colors for cluster
-icl = calcICL(results,title=title,plot="png")
-icl[["clusterConsensus"]]
-icl[["itemConsensus"]][1:5,]
-
-sum(results[[3]]$consensusClass==1)/90
-sum(results[[3]]$consensusClass==2)/90
-sum(results[[3]]$consensusClass==3)/90
